72111c04f3985ad759e34b372b74b635f1576799
gnana.sai.kalupudi <gnana.sai.kalupudi@CDC2-L-11982B3.dir.svc.accenture.com>
2022-05-11 10:45:44 +0530

    push to sai branch

--- simplejava/src/main/java/simplejava/Simple.java
+++ simplejava/src/main/java/simplejava/Simple.java
@@ -4,7 +4,9 @@ public class Simple {
 	public static void main(String[] args)   
 	{    
 		System.out.println("Welcome");  
-		System.out.println("Test");  
+		System.out.println("Test"); 
+		System.out.println("ABC");  
+		System.out.println("ASD"); 
 	}
 
 }
